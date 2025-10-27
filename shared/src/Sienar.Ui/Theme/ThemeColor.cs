a690643da1b963c1e08767fea617674de88d3235
christianlevesque <christian@levesque.dev>
2025-10-27 15:13:21 -0500

    added theme color and variant enums

---
+++ shared/src/Sienar.Ui/Theme/ThemeColor.cs
@@ -0,0 +1,53 @@
+﻿// ReSharper disable once CheckNamespace
+namespace Sienar.Ui;
+
+/// <summary>
+/// The theme colors supported by Sienar
+/// </summary>
+public enum ThemeColor
+{
+	/// <summary>
+	/// The primary color
+	/// </summary>
+	Primary,
+
+	/// <summary>
+	/// The secondary color
+	/// </summary>
+	Secondary,
+
+	/// <summary>
+	/// The tertiary color
+	/// </summary>
+	Tertiary,
+
+	/// <summary>
+	/// A color indicating a success state
+	/// </summary>
+	Success,
+
+	/// <summary>
+	/// A color indicating information
+	/// </summary>
+	Info,
+
+	/// <summary>
+	/// A color indicating a warning state
+	/// </summary>
+	Warning,
+
+	/// <summary>
+	/// A color indicating an error state
+	/// </summary>
+	Error,
+
+	/// <summary>
+	/// The light color
+	/// </summary>
+	Light,
+
+	/// <summary>
+	/// The dark color
+	/// </summary>
+	Dark
+}
---
+++ shared/src/Sienar.Ui/Theme/ThemeVariant.cs
@@ -0,0 +1,23 @@
+﻿// ReSharper disable once CheckNamespace
+namespace Sienar.Ui;
+
+/// <summary>
+/// The solidity variants supported by Sienar
+/// </summary>
+public enum ThemeVariant
+{
+	/// <summary>
+	/// A solid variant; generally filled with a single color and with an offset text color
+	/// </summary>
+	Solid,
+
+	/// <summary>
+	/// An outlined variant; generally similar to the <see cref="Solid"/> variant but with no color fill
+	/// </summary>
+	Outlined,
+
+	/// <summary>
+	/// A text-only variant; generally similar to the <see cref="Outlined"/> variant but with no outline
+	/// </summary>
+	Text
+}
