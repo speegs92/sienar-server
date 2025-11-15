7940364e703e151a06307a104d38e59bd42a7abf
christianlevesque <christian@levesque.dev>
2025-11-15 11:22:55 -0600

    added grid column and row components

---
+++ shared/src/Sienar.Ui/Ui/Column.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Components;
+using Sienar.Html;
+
+namespace Sienar.Ui;
+
+/// <summary>
+/// A Bootstrap-style column component with support for 12 columns and breakpoint-specific column sizes
+/// </summary>
+public class Column : SienarComponentBase
+{
+	/// <summary>
+	/// The column size for the smallest screens
+	/// </summary>
+	[Parameter]
+	public int Col { get; set; }
+
+	/// <summary>
+	/// The column size for small screens
+	/// </summary>
+	[Parameter]
+	public int? Sm { get; set; }
+
+	/// <summary>
+	/// The column size for medium screens
+	/// </summary>
+	[Parameter]
+	public int? Md { get; set; }
+
+	/// <summary>
+	/// The column size for large screens
+	/// </summary>
+	[Parameter]
+	public int? Lg { get; set; }
+
+	/// <summary>
+	/// The column size for extra-large screens
+	/// </summary>
+	[Parameter]
+	public int? Xl { get; set; }
+
+	/// <summary>
+	/// The column size for extra-extra-large screens
+	/// </summary>
+	[Parameter]
+	public int? Xxl { get; set; }
+
+	/// <inheritdoc />
+	protected override void AddCss(CssBuilder b)
+		=> b
+			.AddClass($"col-{Col}")
+			.AddClass($"col-sm-{Sm}", Sm.HasValue)
+			.AddClass($"col-md-{Md}", Md.HasValue)
+			.AddClass($"col-lg-{Lg}", Lg.HasValue)
+			.AddClass($"col-xl-{Xl}", Xl.HasValue)
+			.AddClass($"col-xxl-{Xxl}", Xxl.HasValue);
+}
---
+++ shared/src/Sienar.Ui/Ui/Row.cs
@@ -0,0 +1,13 @@
+﻿using Sienar.Html;
+
+namespace Sienar.Ui;
+
+/// <summary>
+/// A container for Bootstrap-style <see cref="Column">columns</see>
+/// </summary>
+public class Row : SienarComponentBase
+{
+	/// <inheritdoc />
+	protected override void AddCss(CssBuilder b)
+		=> b.AddClass("row");
+}
