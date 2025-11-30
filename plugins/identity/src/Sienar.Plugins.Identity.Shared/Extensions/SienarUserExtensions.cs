c32e1b6d9c23585f56b7dbd2184397d630d95f10
christianlevesque <christian@levesque.dev>
2025-11-30 14:26:12 -0600

    added extension method to determine if a ViewUserDto is locked out

--- plugins/identity/src/Sienar.Plugins.Identity.Shared/Extensions/SienarUserExtensions.cs
+++ plugins/identity/src/Sienar.Plugins.Identity.Shared/Extensions/SienarUserExtensions.cs
@@ -11,6 +11,22 @@ public static class SienarUserExtensions
 		SienarOptions sienarOptions)
 		=> loginOptions.RequireConfirmedAccount && sienarOptions.EnableEmail;
 
+	/// <summary>
+	/// Determines whether the given user is locked out
+	/// </summary>
+	/// <param name="user">The user</param>
+	/// <returns>Whether the user is locked out</returns>
 	public static bool IsLockedOut(this SienarUser user)
-		=> user.LockoutEnd.HasValue && user.LockoutEnd.Value > DateTime.UtcNow;
+		=> IsLockedOutCore(user.LockoutEnd);
+
+	/// <summary>
+	/// Determines whether the given user is locked out
+	/// </summary>
+	/// <param name="user">The user</param>
+	/// <returns>Whether the user is locked out</returns>
+	public static bool IsLockedOut(this ViewUserDto user)
+		=> IsLockedOutCore(user.LockoutEnd);
+
+	private static bool IsLockedOutCore(DateTime? lockoutEnd)
+		=> lockoutEnd.HasValue && lockoutEnd.Value > DateTime.UtcNow;
 }
