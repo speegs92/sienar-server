d8edfc7b0d65728c955fad1a89b44997e0b4f5cc
christianlevesque <christian@levesque.dev>
2025-11-30 21:01:46 -0600

    added standardized <UpsertForm> component wrapping around <StandardForm>

---
+++ shared/src/Sienar.Ui/Ui/Forms/UpsertForm.razor.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.AspNetCore.Components;
+
+// ReSharper disable once CheckNamespace
+namespace Sienar.Ui;
+
+/// <summary>
+/// Automates upserting an entity
+/// </summary>
+/// <typeparam name="TViewDto">The type of the view DTO</typeparam>
+/// <typeparam name="TUpsertDto">The type of the upsert DTO</typeparam>
+public partial class UpsertForm<TViewDto, TUpsertDto>
+{
+	/// <summary>
+	/// The ID of the entity being updated, if any
+	/// </summary>
+	[Parameter]
+	public int? EntityId { get; set; }
+
+	/// <summary>
+	/// A function to retrieve the name of the editing entity
+	/// </summary>
+	[Parameter]
+	public required Func<TUpsertDto, string> For { get; set; }
+
+	/// <summary>
+	/// A function to call when the view-DTO is loaded
+	/// </summary>
+	[Parameter]
+	public Action<TViewDto>? OnLoaded { get; set; }
+
+	/// <summary>
+	/// A function to execute if the add form is submitted successfully
+	/// </summary>
+	/// <remarks>
+	/// The arguments of the provided delegate will be resolved from the DI container. The only exception to this is the <c>OperationResult&lt;int?&gt;</c> returned by <see cref="Sienar.Data.IEntityWriter{TEntity}.Create">IEntityWriter&lt;TEntity&gt;.Create()</see>, which can be provided at any position (but is not required).
+	/// </remarks>
+	[Parameter]
+	public Delegate? OnAddSuccess { get; set; }
+
+	/// <summary>
+	/// A function to execute if the form is submitted successfully
+	/// </summary>
+	/// <remarks>
+	/// The arguments of the provided delegate will be resolved from the DI container. The only exception to this is the <c>OperationResult&lt;bool?&gt;</c> returned by <see cref="Sienar.Data.IEntityWriter{TEntity}.Update">IEntityWriter&lt;TEntity&gt;.Update()</see>, which can be provided at any position (but is not required).
+	/// </remarks>
+	[Parameter]
+	public Delegate? OnUpdateSuccess { get; set; }
+}
+
