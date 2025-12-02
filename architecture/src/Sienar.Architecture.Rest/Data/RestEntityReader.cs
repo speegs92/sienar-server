26043ef9d41fb7709594dede662018065f629b97
christianlevesque <christian@levesque.dev>
2025-12-02 13:48:49 -0600

    updated RestEntityReader to use new IRestClient contract and send notifications to the UI

--- architecture/src/Sienar.Architecture.Rest/Data/RestEntityReader.cs
+++ architecture/src/Sienar.Architecture.Rest/Data/RestEntityReader.cs
@@ -4,7 +4,6 @@ using Microsoft.Extensions.Logging;
 using Sienar.Hooks;
 using Sienar.Infrastructure;
 using Sienar.Security;
-using Sienar.Services;
 
 namespace Sienar.Data;
 
@@ -12,11 +11,12 @@ namespace Sienar.Data;
 /// An implementation of <see cref="IEntityReader{TEntity}"/> which reads entities from a REST endpoint
 /// </summary>
 /// <typeparam name="TDto">The type of the entity to read</typeparam>
-public class RestEntityReader<TDto> : ServiceBase, IEntityReader<TDto>
+public class RestEntityReader<TDto> : IEntityReader<TDto>
 	where TDto : EntityBase
 {
 	private readonly IRestClient _client;
 	private readonly ICrudEndpointGenerator<TDto> _endpointGenerator;
+	private readonly IOperationResultNotifier _notifier;
 	private readonly ILogger<RestEntityReader<TDto>> _logger;
 	private readonly IAccessValidationRunner<TDto> _accessValidationRunner;
 	private readonly IAfterActionRunner<TDto> _afterActionRunner;
@@ -24,23 +24,23 @@ public class RestEntityReader<TDto> : ServiceBase, IEntityReader<TDto>
 	/// <summary>
 	/// Creates a new instance of <c>RestEntityReader</c>
 	/// </summary>
-	/// <param name="notifier">The notifier</param>
 	/// <param name="client">The rest client</param>
 	/// <param name="endpointGenerator">The endpoint generator for the given DTO</param>
+	/// <param name="notifier">The notifier</param>
 	/// <param name="logger">The logger</param>
 	/// <param name="accessValidationRunner">The access validation runner</param>
 	/// <param name="afterActionRunner">The after-action runner</param>
 	public RestEntityReader(
-		INotifier notifier,
 		IRestClient client,
 		ICrudEndpointGenerator<TDto> endpointGenerator,
+		IOperationResultNotifier notifier,
 		ILogger<RestEntityReader<TDto>> logger,
 		IAccessValidationRunner<TDto> accessValidationRunner,
 		IAfterActionRunner<TDto> afterActionRunner)
-		: base(notifier)
 	{
 		_client = client;
 		_endpointGenerator = endpointGenerator;
+		_notifier = notifier;
 		_logger = logger;
 		_accessValidationRunner = accessValidationRunner;
 		_afterActionRunner = afterActionRunner;
@@ -52,17 +52,20 @@ public class RestEntityReader<TDto> : ServiceBase, IEntityReader<TDto>
 		Filter? filter = null)
 	{
 		TDto? entity;
+		OperationResult<WebResult<TDto?>> result;
+
 		try
 		{
 			var endpoint = _endpointGenerator.GenerateReadUrl(id);
-			entity = (await _client.Get<TDto>(
+			result = await _client.Get<TDto>(
 				endpoint,
-				filter)).Result;
+				filter);
+			entity = result.Result?.Result;
 		}
 		catch (Exception e)
 		{
 			_logger.LogError(e, StatusMessages.Database.QueryFailed);
-			return NotifyOfResult(new OperationResult<TDto?>(
+			return _notifier.HandleOperationResult(new OperationResult<TDto?>(
 				OperationStatus.Unknown,
 				null,
 				StatusMessages.Crud<TDto>.ReadSingleFailed()));
@@ -70,7 +73,7 @@ public class RestEntityReader<TDto> : ServiceBase, IEntityReader<TDto>
 
 		if (entity is null)
 		{
-			return NotifyOfResult(new OperationResult<TDto?>(
+			return _notifier.HandleOperationResult(new OperationResult<TDto?>(
 				OperationStatus.NotFound,
 				null,
 				StatusMessages.Crud<TDto>.NotFound(id)));
@@ -80,32 +83,34 @@ public class RestEntityReader<TDto> : ServiceBase, IEntityReader<TDto>
 		var accessValidationResult = await _accessValidationRunner.Validate(entity, ActionType.Read);
 		if (!accessValidationResult.Result)
 		{
-			return NotifyOfResult(new OperationResult<TDto?>(
+			return _notifier.HandleOperationResult(new OperationResult<TDto?>(
 				OperationStatus.Unauthorized,
 				null,
 				StatusMessages.Crud<TDto>.NoPermission()));
 		}
 
 		await _afterActionRunner.Run(entity, ActionType.Read);
-		return NotifyOfResult(new OperationResult<TDto?>(result: entity));
+		return _notifier.HandleWebResult(result);
 	}
 
 	/// <inheritdoc />
 	public async Task<OperationResult<PagedQueryResult<TDto>>> Read(Filter? filter = null)
 	{
+		OperationResult<WebResult<PagedQueryResult<TDto>?>> webResult;
 		PagedQueryResult<TDto> queryResult;
 
 		try
 		{
 			var endpoint = _endpointGenerator.GenerateReadUrl();
-			queryResult = (await _client.Get<PagedQueryResult<TDto>>(
+			webResult = await _client.Get<PagedQueryResult<TDto>>(
 				endpoint,
-				filter)).Result ?? new();
+				filter);
+			queryResult = webResult.Result?.Result ?? new PagedQueryResult<TDto>();
 		}
 		catch (Exception e)
 		{
 			_logger.LogError(e, StatusMessages.Database.QueryFailed);
-			return NotifyOfResult(new OperationResult<PagedQueryResult<TDto>>(
+			return _notifier.HandleOperationResult(new OperationResult<PagedQueryResult<TDto>>(
 				OperationStatus.Unknown,
 				new PagedQueryResult<TDto>(),
 				StatusMessages.Crud<TDto>.ReadMultipleFailed()));
@@ -116,6 +121,6 @@ public class RestEntityReader<TDto> : ServiceBase, IEntityReader<TDto>
 			await _afterActionRunner.Run(entity, ActionType.ReadAll);
 		}
 
-		return NotifyOfResult(new OperationResult<PagedQueryResult<TDto>>(result: queryResult));
+		return _notifier.HandleWebResult(webResult);
 	}
 }
