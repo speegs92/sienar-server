fe911b0ece8365f3b9e82f10f24984069e09995c
christianlevesque <christian@levesque.dev>
2025-12-03 12:40:44 -0600

    updated ClientManuallyConfirmUserAccountProcessor to use new IRestClient contract

--- plugins/identity/src/Sienar.Plugins.Identity.Client/Identity/Processors/ClientManuallyConfirmUserAccountProcessor.cs
+++ plugins/identity/src/Sienar.Plugins.Identity.Client/Identity/Processors/ClientManuallyConfirmUserAccountProcessor.cs
@@ -1,7 +1,6 @@
 ﻿#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
 
 using System.Threading.Tasks;
-using Sienar.Data;
 using Sienar.Identity.Requests;
 using Sienar.Infrastructure;
 using Sienar.Processors;
@@ -12,13 +11,18 @@ namespace Sienar.Identity.Processors;
 public class ClientManuallyConfirmUserAccountProcessor : IStatusProcessor<ManuallyConfirmUserAccountRequest>
 {
 	private readonly IRestClient _client;
+	private readonly IOperationResultNotifier _notifier;
 
-	public ClientManuallyConfirmUserAccountProcessor(IRestClient client)
+	public ClientManuallyConfirmUserAccountProcessor(
+		IRestClient client,
+		IOperationResultNotifier notifier)
 	{
 		_client = client;
+		_notifier = notifier;
 	}
 
-	public Task<OperationResult<bool>> Process(
+	public async Task<OperationResult<bool>> Process(
 		ManuallyConfirmUserAccountRequest request)
-		=> _client.Patch<bool>("users/confirm", request);
+		=> _notifier.HandleWebResult(
+			await _client.Patch<bool>("users/confirm", request));
 }
