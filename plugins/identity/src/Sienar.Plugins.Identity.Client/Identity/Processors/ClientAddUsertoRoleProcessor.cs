723a6131542a910d5f372041775e050900fc233f
christianlevesque <christian@levesque.dev>
2025-12-03 12:28:03 -0600

    updated ClientAddUserToRoleProcessor to use new IRestClient contract

--- plugins/identity/src/Sienar.Plugins.Identity.Client/Identity/Processors/ClientAddUsertoRoleProcessor.cs
+++ plugins/identity/src/Sienar.Plugins.Identity.Client/Identity/Processors/ClientAddUsertoRoleProcessor.cs
@@ -1,7 +1,6 @@
 ﻿#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
 
 using System.Threading.Tasks;
-using Sienar.Data;
 using Sienar.Identity.Requests;
 using Sienar.Infrastructure;
 using Sienar.Processors;
@@ -12,12 +11,17 @@ namespace Sienar.Identity.Processors;
 public class ClientAddUsertoRoleProcessor : IStatusProcessor<AddUserToRoleRequest>
 {
 	private readonly IRestClient _client;
+	private readonly IOperationResultNotifier _notifier;
 
-	public ClientAddUsertoRoleProcessor(IRestClient client)
+	public ClientAddUsertoRoleProcessor(
+		IRestClient client,
+		IOperationResultNotifier notifier)
 	{
 		_client = client;
+		_notifier = notifier;
 	}
 
-	public Task<OperationResult<bool>> Process(AddUserToRoleRequest request)
-		=> _client.Post<bool>("users/roles", request);
+	public async Task<OperationResult<bool>> Process(AddUserToRoleRequest request)
+		=> _notifier.HandleWebResult(
+			await _client.Post<bool>("users/roles", request));
 }
