40d12bbb5ea6f97f5aeb693135d50072d2f1768d
christianlevesque <christian@levesque.dev>
2025-12-04 12:39:26 -0600

    Split IEntityReader into IEntityReadActor and IEntityReadAllActor

---
+++ shared/src/Sienar.Utils/Data/IEntityReadAllActor.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading.Tasks;
+using Sienar.Infrastructure;
+
+namespace Sienar.Data;
+
+/// <summary>
+/// A service to read multiple instances of <c>T</c> from the appropriate repository
+/// </summary>
+/// <typeparam name="T">The type of the entity</typeparam>
+public interface IEntityReadAllActor<T>
+{
+	/// <summary>
+	/// Gets multiple entities
+	/// </summary>
+	/// <param name="filter">A <see cref="Filter"/> to specify included results</param>
+	/// <returns>A list of all entities in the database</returns>
+	Task<OperationResult<PagedQueryResult<T>>> Read(
+		Filter? filter = null);
+}
