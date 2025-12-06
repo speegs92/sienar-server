3b47dd426f06a243f8c4a7832d967729fc424cad
christianlevesque <christian@levesque.dev>
2025-12-06 11:42:47 -0600

    added IAfterReadAllAction to serve as hook type to run after a read-all action

---
+++ shared/src/Sienar.Utils/Data/IAfterReadAllAction.cs
@@ -0,0 +1,10 @@
+﻿using Sienar.Hooks;
+
+namespace Sienar.Data;
+
+/// <summary>
+/// Performs arbitrary work after a read-all action has already run
+/// </summary>
+/// <typeparam name="T">The type of the entity</typeparam>
+public interface IAfterReadAllAction<T> : IAfterActionBase<T>
+	where T : EntityBase;
