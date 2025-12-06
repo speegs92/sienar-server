9b72fff64b112296af3343fb88b20ff3baaecb42
christianlevesque <christian@levesque.dev>
2025-12-06 13:11:10 -0600

    added new IAfterGeneralAction to serve as hook type to run after a delete action

---
+++ shared/src/Sienar.Utils/Services/IAfterGeneralAction.cs
@@ -0,0 +1,10 @@
+﻿using Sienar.Hooks;
+
+namespace Sienar.Services;
+
+/// <summary>
+/// Performs arbitrary work after a general action has already run
+/// </summary>
+/// <typeparam name="T">The type of the request</typeparam>
+public interface IAfterGeneralAction<T> : IAfterActionBase<T>
+	where T : IRequest;
