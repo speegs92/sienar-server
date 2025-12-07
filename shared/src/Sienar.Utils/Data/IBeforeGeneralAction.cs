9b21d04bed9dff35316d5ef9d8c8da213c8ef8e9
christianlevesque <christian@levesque.dev>
2025-12-06 22:55:11 -0600

    added missing XMLDoc typeparam

--- shared/src/Sienar.Utils/Data/IBeforeGeneralAction.cs
+++ shared/src/Sienar.Utils/Data/IBeforeGeneralAction.cs
@@ -5,6 +5,6 @@ namespace Sienar.Data;
 /// <summary>
 /// Performs arbitrary actions before a general action has run
 /// </summary>
-/// <typeparam name="T"></typeparam>
+/// <typeparam name="T">The type of the request</typeparam>
 public interface IBeforeGeneralAction<T> : IBeforeActionBase<T>
 	where T : IRequest;
