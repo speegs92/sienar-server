096038bdb6da0bfd3c543adb27caf49d7a7a5d37
christianlevesque <christian@levesque.dev>
2025-12-07 00:11:06 -0600

    updated non-file-related direct usages of IResultActor to instead use IResultActionOrchestrator

--- plugins/identity/src/Sienar.Plugins.Identity.Server/Identity/AccountController.cs
+++ plugins/identity/src/Sienar.Plugins.Identity.Server/Identity/AccountController.cs
@@ -33,8 +33,8 @@ public class AccountController : SienarController
 	[HttpGet]
 	[UsedImplicitly]
 	public Task<IActionResult> GetAccountData(
-		[FromServices] IResultActor<AccountDataResult> actor)
-		=> Execute(actor.Execute);
+		[FromServices] IResultActionOrchestrator<AccountDataResult> orchestrator)
+		=> orchestrator.Execute();
 
 	[HttpDelete]
 	[UsedImplicitly]
