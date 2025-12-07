123d9a0dc2a97f2ca7481b1386b76ed936fa41ae
christianlevesque <christian@levesque.dev>
2025-12-06 22:20:57 -0600

    added IBeforeUpdateAction to serve as hook type to run before an update action

---
+++ shared/src/Sienar.Utils/Data/IBeforeUpdateAction.cs
@@ -0,0 +1,13 @@
+﻿using Sienar.Hooks;
+
+namespace Sienar.Data;
+
+/// <summary>
+/// Performs arbitrary actions before an update action has run
+/// </summary>
+/// <remarks>
+/// When this hook is called, the instance of <c>T</c> has not yet been updated in the database, so exercise care when querying the database, adding related entities, or any other database-related work.
+/// </remarks>
+/// <typeparam name="T">The type of the entity</typeparam>
+public interface IBeforeUpdateAction<T> : IBeforeActionBase<T>
+	where T : EntityBase;
