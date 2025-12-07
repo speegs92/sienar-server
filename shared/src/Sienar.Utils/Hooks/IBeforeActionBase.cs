a70a74b45de4601497d97c9948ee6b3b8258b91a
christianlevesque <christian@levesque.dev>
2025-12-06 21:01:47 -0600

    added new IBeforeActionBase interface, which serves as a base interface for all before-hooks

---
+++ shared/src/Sienar.Utils/Hooks/IBeforeActionBase.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Tasks;
+
+namespace Sienar.Hooks;
+
+/// <summary>
+/// Performs arbitrary actions before a processor has run
+/// </summary>
+/// <typeparam name="T">The type of the request or entity</typeparam>
+// ReSharper disable once TypeParameterCanBeVariant
+public interface IBeforeActionBase<T>
+{
+	/// <summary>
+	/// Performs arbitrary actions before a processor has run
+	/// </summary>
+	/// <param name="request">The request or entity</param>
+	Task Handle(T request);
+}
