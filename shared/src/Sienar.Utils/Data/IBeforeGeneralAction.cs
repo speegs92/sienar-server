8e34a5273608bec60bd0678b37d5320a8f3d32c2
christianlevesque <christian@levesque.dev>
2025-12-06 22:50:37 -0600

    added IBeforeGeneralAction to serve as hook type to run before a general action

---
+++ shared/src/Sienar.Utils/Data/IBeforeGeneralAction.cs
@@ -0,0 +1,10 @@
+﻿using Sienar.Hooks;
+
+namespace Sienar.Data;
+
+/// <summary>
+/// Performs arbitrary actions before a general action has run
+/// </summary>
+/// <typeparam name="T"></typeparam>
+public interface IBeforeGeneralAction<T> : IBeforeActionBase<T>
+	where T : IRequest;
