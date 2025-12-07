381ab65eeacf3b481ac3db85317a50c9ebe48f8c
christianlevesque <christian@levesque.dev>
2025-12-07 15:45:52 -0600

    created startup processor for use by SienarAppBuilder

---
+++ shared/src/Sienar.Utils/Infrastructure/Startup.cs
@@ -0,0 +1,6 @@
+﻿namespace Sienar.Infrastructure;
+
+/// <summary>
+/// A request used to run any startup configuration that isn't directly provided by Sienar
+/// </summary>
+public class Startup : IRequest;
