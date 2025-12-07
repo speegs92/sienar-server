7ace017f41cddf91208981f76a41de671166ffab
christianlevesque <christian@levesque.dev>
2025-12-06 22:55:26 -0600

    added IBeforeStatusAction to serve as hook type to run before a status action

---
+++ shared/src/Sienar.Utils/Data/IBeforeStatusAction.cs
@@ -0,0 +1,10 @@
+﻿using Sienar.Hooks;
+
+namespace Sienar.Data;
+
+/// <summary>
+/// Performs arbitrary actions before a status action has run
+/// </summary>
+/// <typeparam name="T">The type of the request</typeparam>
+public interface IBeforeStatusAction<T> : IBeforeActionBase<T>
+	where T : IRequest;
