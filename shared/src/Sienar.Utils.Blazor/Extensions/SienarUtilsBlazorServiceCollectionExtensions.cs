a6756ef73402da033d6e37b0218c08c071e32d62
christianlevesque <christian@levesque.dev>
2025-12-08 11:53:09 -0600

    removed SienarStartupActor component and AddStartupTask extension method, since this is now handled by IBeforeTask<Startup>

--- shared/src/Sienar.Utils.Blazor/Extensions/SienarUtilsBlazorServiceCollectionExtensions.cs
+++ shared/src/Sienar.Utils.Blazor/Extensions/SienarUtilsBlazorServiceCollectionExtensions.cs
@@ -20,14 +20,4 @@ public static class SienarUtilsBlazorServiceCollectionExtensions
 
 		return self;
 	}
-
-	/// <summary>
-	/// Adds a task to run once the Blazor UI has rendered and is ready to execute JavaScript
-	/// </summary>
-	/// <param name="self">the service collection</param>
-	/// <typeparam name="T">The type of the startup task</typeparam>
-	/// <returns>the service collection</returns>
-	public static IServiceCollection AddStartupTask<T>(this IServiceCollection self)
-		where T : class, IBeforeTask<SienarStartupActor>
-		=> self.AddScoped<IBeforeTask<SienarStartupActor>, T>();
 }
