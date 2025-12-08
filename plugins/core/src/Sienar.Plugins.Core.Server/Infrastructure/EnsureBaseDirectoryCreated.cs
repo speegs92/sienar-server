6f5cc14cde01acd383f1b148615c5e91598d1486
christianlevesque <christian@levesque.dev>
2025-12-08 11:37:18 -0600

    moved base directory creation to a startup action

---
+++ plugins/core/src/Sienar.Plugins.Core.Server/Infrastructure/EnsureBaseDirectoryCreated.cs
@@ -0,0 +1,20 @@
+﻿using System.IO;
+
+namespace Sienar.Infrastructure;
+
+/// <summary>
+/// Ensures that the web application's files directory exists at app startup
+/// </summary>
+public class EnsureBaseDirectoryCreated : IBeforeStatusAction<Startup>
+{
+	/// <inheritdoc />
+	public Task Handle(Startup request)
+	{
+		FileUtils.BaseAppFolderName = Path.Combine(
+			Environment.CurrentDirectory,
+			"../SienarFiles");
+		FileUtils.EnsureDirectoryExists(FileUtils.BaseAppFolderName);
+
+		return Task.CompletedTask;
+	}
+}
