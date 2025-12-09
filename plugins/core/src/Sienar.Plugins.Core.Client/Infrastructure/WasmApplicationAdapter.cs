455674099aa9840e76956e41ac5b4304bf7fc2e5
christianlevesque <christian@levesque.dev>
2025-12-09 12:10:26 -0600

    reintroduced <SienarStartupActor> to call startup initialization code and removed startup initialization from WasmApplicationAdapter

--- plugins/core/src/Sienar.Plugins.Core.Client/Infrastructure/WasmApplicationAdapter.cs
+++ plugins/core/src/Sienar.Plugins.Core.Client/Infrastructure/WasmApplicationAdapter.cs
@@ -33,17 +33,9 @@ public class WasmApplicationAdapter : IApplicationAdapter<WebAssemblyHostBuilder
 	}
 
 	/// <inheritdoc />
-	public async Task<T> Build<T>(IServiceProvider sp)
+	public Task<T> Build<T>(IServiceProvider sp)
 		where T : class
-	{
-		var app = Builder.Build();
-		await using var scope = app.Services.CreateAsyncScope();
-
-		var startupActor = scope.ServiceProvider.GetRequiredService<IStatusActor<Startup>>();
-		await startupActor.Execute(new Startup());
-
-		return (app as T)!;
-	}
+		=> Task.FromResult((Builder.Build() as T)!);
 
 	/// <inheritdoc />
 	public void AddServices(Action<IServiceCollection> configurer)
---
+++ shared/src/Sienar.Utils.Blazor/Ui/SienarStartupActor.cs
@@ -0,0 +1,16 @@
+﻿namespace Sienar.Ui;
+
+/// <summary>
+/// A component used to call startup initialization tasks after the initial render
+/// </summary>
+public class SienarStartupActor : ComponentBase
+{
+	[Inject]
+	private IStatusActor<Startup> StartupActor { get; set; } = null!;
+
+	/// <inheritdoc />
+	protected override async Task OnInitializedAsync()
+	{
+		await StartupActor.Execute(new Startup());
+	}
+}
