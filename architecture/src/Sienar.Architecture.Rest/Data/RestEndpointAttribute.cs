bb37c56bdca00a604b46d298fb46ad6a8ad2b807
christianlevesque <christian@levesque.dev>
2025-12-15 16:36:59 -0600

    updated [RestEndpoint] to allow for an optional Method property

--- architecture/src/Sienar.Architecture.Rest/Data/RestEndpointAttribute.cs
+++ architecture/src/Sienar.Architecture.Rest/Data/RestEndpointAttribute.cs
@@ -12,9 +12,44 @@ public class RestEndpointAttribute : Attribute
 	public string Endpoint { get; }
 
 	/// <summary>
+	/// The HTTP method
+	/// </summary>
+	public HttpMethod? Method { get; }
+
+	/// <summary>
 	/// Creates a new instance of <c>RestEndpointAttribute</c>
 	/// </summary>
-	/// <param name="endpoint"></param>
-	public RestEndpointAttribute(string endpoint)
-		=> Endpoint = endpoint;
+	/// <param name="endpoint">The endpoint at which the DTO should interact</param>
+	/// <param name="method">The HTTP method with which the DTO should interact</param>
+	public RestEndpointAttribute(
+		string endpoint,
+		string? method = null)
+	{
+		Endpoint = endpoint;
+		Method = MapToHttpMethod(method);
+	}
+
+	private static HttpMethod? MapToHttpMethod(string? method)
+	{
+		if (string.IsNullOrEmpty(method))
+		{
+			return null;
+		}
+
+		return method switch
+		{
+			HttpMethods.Connect => HttpMethod.Connect,
+			HttpMethods.Delete => HttpMethod.Delete,
+			HttpMethods.Get => HttpMethod.Get,
+			HttpMethods.Head => HttpMethod.Head,
+			HttpMethods.Options => HttpMethod.Options,
+			HttpMethods.Patch => HttpMethod.Patch,
+			HttpMethods.Post => HttpMethod.Post,
+			HttpMethods.Put => HttpMethod.Put,
+			HttpMethods.Trace => HttpMethod.Trace,
+			_ => throw new ArgumentException(
+				"The provided HTTP method name must be one of the valid HTTP verbs",
+				nameof(method))
+		};
+	}
 }
