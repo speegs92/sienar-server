07a392d2c8d0bcf69435a7c0437743c81a6d06f4
christianlevesque <christian@levesque.dev>
2025-12-15 13:05:55 -0600

    added overload of IServiceCollection.TryAddScoped() which accepts an ApplicationType argument, which is used to determine which scope is appropriate for the given application type

--- shared/src/Sienar.Utils/Extensions/SienarUtilsServiceCollectionExtensions.cs
+++ shared/src/Sienar.Utils/Extensions/SienarUtilsServiceCollectionExtensions.cs
@@ -422,6 +422,33 @@ public static class SienarUtilsServiceCollectionExtensions
 		return serviceCollection;
 	}
 
+	/// <summary>
+	/// Tries to add a scoped service in a way that supports the application type of the app registering the service
+	/// </summary>
+	/// <remarks>
+	/// This method inspects the <see cref="ApplicationType"/> and adds the given service in a way which is closest to the idea of a scoped-lifetime service for the given <see cref="ApplicationType"/>. An <see cref="ApplicationType.Client">ApplicationType.Client</see>, for example, will add such a service as <see cref="ServiceLifetime.Transient">ServiceLifetime.Transient</see> because Blazor WASM does not functionally distinguish between scoped- and singleton-lifetime services.
+	/// </remarks>
+	/// <param name="serviceCollection">The service collection</param>
+	/// <param name="appType">The application type</param>
+	/// <typeparam name="TService">The type of the service</typeparam>
+	/// <typeparam name="TImplementation">The type of the service's implementation</typeparam>
+	/// <returns>The service collection</returns>
+	public static IServiceCollection TryAddScoped<TService, TImplementation>(
+		IServiceCollection serviceCollection,
+		ApplicationType appType)
+	{
+		var lifetime = CreateTraditionallyScopedServiceLifetime(appType);
+
+		var descriptor = new ServiceDescriptor(
+			typeof(TService),
+			typeof(TImplementation),
+			lifetime);
+
+		serviceCollection.TryAdd(descriptor);
+
+		return serviceCollection;
+	}
+
 	private static ServiceLifetime CreateTraditionallyScopedServiceLifetime(ApplicationType appType)
 		=> appType switch
 		{
