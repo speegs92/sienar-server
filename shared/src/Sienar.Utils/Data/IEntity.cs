809309810e05bcc19f158d61ea601e2592570b71
christianlevesque <christian@levesque.dev>
2025-12-22 19:51:13 -0600

    added new IEntity to define base entity properties in favor of EntityBase

---
+++ shared/src/Sienar.Utils/Data/IEntity.cs
@@ -0,0 +1,17 @@
+﻿namespace Sienar.Data;
+
+/// <summary>
+/// A basic Sienar entity
+/// </summary>
+public interface IEntity
+{
+	/// <summary>
+	/// The entity's primary key
+	/// </summary>
+	public int Id { get; set; }
+
+	/// <summary>
+	/// The entity's concurrency stamp
+	/// </summary>
+	public Guid ConcurrencyStamp { get; set; }
+}
