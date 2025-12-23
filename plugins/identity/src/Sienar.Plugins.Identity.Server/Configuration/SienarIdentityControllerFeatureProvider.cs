9f91c90f8b1d2c4c84107ffd53244f62faa4c33c
christianlevesque <christian@levesque.dev>
2025-12-23 13:53:04 -0600

    updated test project server app to add generic Sienar controllers

---
+++ plugins/identity/src/Sienar.Plugins.Identity.Server/Configuration/SienarIdentityControllerFeatureProvider.cs
@@ -0,0 +1,20 @@
+﻿using System.Reflection;
+using Microsoft.AspNetCore.Mvc.ApplicationParts;
+using Microsoft.AspNetCore.Mvc.Controllers;
+
+namespace Sienar.Configuration;
+
+/// <summary>
+/// Adds Sienar Identity generic controllers to the controller feature provider
+/// </summary>
+/// <typeparam name="T">The type of the user entity</typeparam>
+public class SienarIdentityControllerFeatureProvider<T> : IApplicationFeatureProvider<ControllerFeature>
+	where T : class, ISienarIdentityUser<T>, new()
+{
+	/// <inheritdoc />
+	public void PopulateFeature(IEnumerable<ApplicationPart> parts, ControllerFeature feature)
+	{
+		feature.Controllers.Add(typeof(UsersController<T>).GetTypeInfo());
+		feature.Controllers.Add(typeof(LockoutReasonController<T>).GetTypeInfo());
+	}
+}
