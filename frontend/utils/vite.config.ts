1fe888677f4138ed796ddac16d0922b820644cce
speegs92 <christian@levesque.dev>
2026-01-09 13:27:38 -0600

    added stub @sienar/utils package

---
+++ frontend/utils/vite.config.ts
@@ -0,0 +1,34 @@
+﻿import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import dts from 'vite-plugin-dts';
+
+const external = [
+	'vue',
+	'vue-router'
+];
+
+// https://vite.dev/config/
+export default defineConfig({
+	build: {
+		lib: {
+			entry: {
+				'index': './src/index.ts'
+			},
+			formats: [ 'es' ]
+		},
+		rollupOptions: { external }
+	},
+	esbuild: {
+		minifyIdentifiers: false
+	},
+	optimizeDeps: {
+		exclude: external
+	},
+	plugins: [
+		vue(),
+		dts({
+			rollupTypes: true,
+			tsconfigPath: './tsconfig.app.json'
+		})
+	]
+});
