aae6f98625402bddf5881c411b448430bc6d6ec9
speegs92 <christian@levesque.dev>
2026-01-18 12:58:42 -0600

    added global component registration to @sienar/ui

--- frontend/playground/src/main.ts
+++ frontend/playground/src/main.ts
@@ -1,8 +1,11 @@
 import { addAppConfigurator, createRouter, createSienarApp, provide, registerRoutes } from '@sienar/utils';
+import { setup as sienarUiSetup } from '@sienar/ui';
 import HomeView from '@/views/Home.vue';
 import MainLayout from '@/layouts/MainLayout.vue';
 
 import type { Component, InjectionKey } from 'vue';
+
+sienarUiSetup();
 
 const MAIN_LAYOUT = Symbol() as InjectionKey<Component>;
 provide(MAIN_LAYOUT, MainLayout);
--- frontend/ui/src/setup.ts
+++ frontend/ui/src/setup.ts
@@ -1,5 +1,20 @@
+import { addAppConfigurator } from '@sienar/utils';
+import * as components from '@ui/components/index.ts';
 import '@ui/styles/utilities/index.scss';
 
-export default function sienarUiSetup() {
+import type { App } from 'vue';
 
+export default function sienarUiSetup() {
+	addAppConfigurator(registerComponents);
+}
+
+function registerComponents(app: App<Element>) {
+	app
+		.component('Container', components.Container);
+}
+
+declare module 'vue' {
+	export interface GlobalComponents {
+		Container: typeof components.Container
+	}
 }
