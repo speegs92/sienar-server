1bc40778066329dd7a48a1d174e64cbc8c1fc450
speegs92 <christian@levesque.dev>
2026-04-21 14:02:28 -0500

    saving work

---
+++ shared/src/Sienar.Ui/Ui/Card.razor.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Components;
+using Sienar.Html;
+using Sienar.Themes;
+
+namespace Sienar.Ui;
+
+public partial class Card
+{
+	private string? Css => new CssBuilder()
+		.AddClass("card")
+		.AddClass($"card--{ThemeUtilities.GetCssString(Color)}", Color is not null)
+		.AddClass($"card--{ThemeUtilities.GetCssString(Variant)}", Color is not null) // If there's no color, the variant doesn't do anything
+		.Build();
+
+	[Parameter]
+	public Color? Color { get; set; }
+
+	[Parameter]
+	public Variant Variant { get; set; } = Variant.Solid;
+
+	public Card() => Tag = "article";
+}
