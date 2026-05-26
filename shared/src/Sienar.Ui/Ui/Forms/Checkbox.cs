f5a9060420cd995a46707257e39bb8fc691d02af
speegs92 <christian@levesque.dev>
2026-05-26 17:41:07 -0500

    added variants and xmldoc comments to form fields

--- shared/src/Sienar.Ui/Ui/Forms/Checkbox.cs
+++ shared/src/Sienar.Ui/Ui/Forms/Checkbox.cs
@@ -1,8 +1,13 @@
 ﻿// ReSharper disable once CheckNamespace
 namespace Sienar.Ui;
 
+/// <summary>
+/// A configured version of <see cref="MudCheckBox{T}"/>
+/// </summary>
+/// <typeparam name="T">The type of the input value</typeparam>
 public class Checkbox<T> : MudCheckBox<T>
 {
+	/// <inheritdoc />
 	public Checkbox()
 	{
 		Color = Color.Primary;
--- shared/src/Sienar.Ui/Ui/Forms/Radio.cs
+++ shared/src/Sienar.Ui/Ui/Forms/Radio.cs
@@ -1,6 +1,10 @@
 ﻿// ReSharper disable once CheckNamespace
 namespace Sienar.Ui;
 
+/// <summary>
+/// A configured version of <see cref="MudRadio{T}"/>
+/// </summary>
+/// <typeparam name="T">The type of the input value</typeparam>
 public class Radio<T> : MudRadio<T>
 {
 	/// <inheritdoc />
