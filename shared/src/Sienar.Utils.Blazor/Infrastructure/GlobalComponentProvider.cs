481eeb140ce3eb30ac6baa36e0de495a30dd42e9
speegs92 <christian@levesque.dev>
2026-05-26 13:56:04 -0500

    made component types not optional

--- shared/src/Sienar.Utils.Blazor/Infrastructure/GlobalComponentProvider.cs
+++ shared/src/Sienar.Utils.Blazor/Infrastructure/GlobalComponentProvider.cs
@@ -8,15 +8,15 @@ public class GlobalComponentProvider
 	/// <summary>
 	/// The default layout component to use when no layout is specified
 	/// </summary>
-	public Type? DefaultLayout { get; set; }
+	public required Type DefaultLayout { get; set; }
 
 	/// <summary>
 	/// The component to render when no route is matched by the router
 	/// </summary>
-	public Type? NotFoundView { get; set; }
+	public required Type NotFoundView { get; set; }
 
 	/// <summary>
 	/// The component to render when the user is fails an authorization check
 	/// </summary>
-	public Type? UnauthorizedView { get; set; }
+	public required Type UnauthorizedView { get; set; }
 }
