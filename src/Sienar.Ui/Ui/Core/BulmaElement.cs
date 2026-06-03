bde658a00ee15b923c609d768db3db54b84bea46
speegs92 <christian@levesque.dev>
2026-06-03 16:26:42 -0500

    added basic base bulma element

---
+++ src/Sienar.Ui/Ui/Core/BulmaElement.cs
@@ -0,0 +1,20 @@
+﻿// ReSharper disable once CheckNamespace
+namespace Sienar.Ui;
+
+/// <summary>
+/// A base Bulma element
+/// </summary>
+public abstract class BulmaElement : ComponentBase
+{
+	/// <summary>
+	/// The HTML tag with which to render the Bulma element
+	/// </summary>
+	[Parameter]
+	public string? Tag { get; set; }
+
+	/// <summary>
+	/// The HTML attributes to add to the rendered HTML
+	/// </summary>
+	[Parameter]
+	public IReadOnlyDictionary<string, object>? Attributes { get; set; }
+}
